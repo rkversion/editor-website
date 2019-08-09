22f442478f955a7bde9e4e7481bc779154f04c03
Tim Waters <chippy2005@gmail.com>
2019-08-09 19:04:58 +0100

    update vagrant provision file to use bundle exec and fix for tests using phantomjs

--- script/vagrant/setup/provision.sh
+++ script/vagrant/setup/provision.sh
@@ -49,7 +49,10 @@ popd
 if [ ! -f config/database.yml ]; then
     sudo -u vagrant cp config/example.database.yml config/database.yml
 fi
+if [ ! -f config/storage.yml ]; then
+    cp config/example.storage.yml config/storage.yml
+fi
 touch config/settings.local.yml
 # migrate the database to the latest version
-sudo -u vagrant rake db:migrate
+sudo -u vagrant bundle exec rake db:migrate
 popd
