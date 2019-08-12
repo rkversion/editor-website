de1426e46b37281687b0095ff2d0b359d97bfd48
Tim Waters <chippy2005@gmail.com>
2019-08-12 15:35:39 +0100

    fix for rubocop formatting

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,6 @@ require "capybara/poltergeist"
 # https://github.com/ariya/phantomjs/issues/14376
 ENV["QT_QPA_PLATFORM"] = "offscreen" if IO.popen(["phantomjs", "--version"], :err => :close).read.empty?
 
-
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
   ActionDispatch::SystemTesting::Server.silence_puma = true
 end
