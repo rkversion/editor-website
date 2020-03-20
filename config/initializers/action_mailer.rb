d84e4c41a95c3baa7746408d5dd0cc6fc7bcbbd3
Tim Waters <chippy2005@gmail.com>
2020-03-20 13:04:04 +0000

    dont set action mailer deliver method in non test environments

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,20 +1,22 @@
 # Configure ActionMailer SMTP settings
-if Settings.key?(:sendgrid_api_key)
-  ActionMailer::Base.delivery_method = :sendgrid_actionmailer
-  ActionMailer::Base.sendgrid_actionmailer_settings = {
-      api_key: Settings.sendgrid_api_key,
-      raise_delivery_errors: true
-  }
-else 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address => Settings.smtp_address || "localhost",
-    :port => Settings.smtp_port || 25,
-    :user_name => Settings.smtp_username,
-    :password => Settings.smtp_password,
-    :domain => Settings.smtp_domain || "localhost",
-    :enable_starttls_auto => false
-  }
+unless Rails.env.test? 
+  if Settings.key?(:sendgrid_api_key)
+    ActionMailer::Base.delivery_method = :sendgrid_actionmailer
+    ActionMailer::Base.sendgrid_actionmailer_settings = {
+        api_key: Settings.sendgrid_api_key,
+        raise_delivery_errors: true
+    }
+  else 
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :address => Settings.smtp_address || "localhost",
+      :port => Settings.smtp_port || 25,
+      :user_name => Settings.smtp_username,
+      :password => Settings.smtp_password,
+      :domain => Settings.smtp_domain || "localhost",
+      :enable_starttls_auto => false
+    }
+  end
 end
 # Set the host and protocol for all ActionMailer URLs
 ActionMailer::Base.default_url_options = {
