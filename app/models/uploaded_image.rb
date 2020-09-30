ffcc5fe66ce1542602bb140f325942b0df1aff1b
bladehan2020 <bladehan@google.com>
2020-09-30 16:54:15 -0400

    for MyImages, use whatdoihave api to get images for the current user

--- app/models/uploaded_image.rb
+++ app/models/uploaded_image.rb
@@ -1,7 +1,11 @@
 class UploadedImage < ActiveRecord::Base
-  def self.get_uploaded_images
-    uri = URI.parse('http://noter-backend:3001/api/v0.1/images/')
-    response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body)
+  def self.get_uploaded_images(user)
+    uri = URI.parse('http://noter-backend:3001/api/v0.1/whatdoihave/')
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.path)
+    req["X-Email"] = user.email
+    res = http.request(req)
+    user_has = JSON.parse(res.body)
+    return user_has['images_by_user']
   end
 end
